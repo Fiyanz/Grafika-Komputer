d97790e4259c6b2418f0de5be0dbd84635bcd1d4
Fiyanz <dctbagalyus@gmail.com>
2024-11-13 23:40:53 +0700

    ploting points

--- bezier/main.py
+++ bezier/main.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import modul.bezier as bzr
 
 
-def plot_bzier_liner(bzr: list) -> None:
+def plot_bzier(bzr: list) -> None:
 
     x_coords = [point[0] for point in bzr]
     y_coords = [point[1] for point in bzr]
 
     plt.figure(figsize=(6, 6))
     plt.plot(x_coords, y_coords, marker='o', color='blue', markersize=5, linestyle='-', linewidth=1)
-    plt.title("Plot Titik Perpindahan Algoritma Bezier Liner")
+    plt.title("Plot Titik Perpindahan Algoritma Bezier")
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.grid(True)
@@ -17,11 +17,31 @@ def plot_bzier_liner(bzr: list) -> None:
     plt.show()
 
 
+
 if __name__ == "__main__":
-    delta_t = 0.2
+    delta_t = 0.02
     points = [
         (1,4),
         (5,9)
     ]
+
+    points_kuadratik = [
+        (3, 1), 
+        (5, 3), 
+        (10, 12)
+    ]
+
+    points_kubik = [
+        (0, 3),
+        (-4, 5),
+        (10, 2),
+        (4, -2),
+        (-1, 0)
+    ]
+
     bzr_liner = bzr.linier(delta_t, points)
-    plot_bzier_liner(bzr_liner)
+    bzr_kuadratik = bzr.kuadratik(delta_t, points_kuadratik)
+    bzr_kubik = bzr.kubik(delta_t, points_kubik)
+    # plot_bzier(bzr_liner)
+    # plot_bzier(bzr_kuadratik)
+    plot_bzier(bzr_kubik)
